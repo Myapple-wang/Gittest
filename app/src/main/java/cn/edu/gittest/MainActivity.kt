7bd1cf0df1e4a53568e49612137141409235d31c
Myapple-wang <2081429707@qq.com>
2020-12-27 14:25:12 +0800

    third

--- app/src/main/java/cn/edu/gittest/MainActivity.kt
+++ app/src/main/java/cn/edu/gittest/MainActivity.kt
@@ -1,11 +1,56 @@
 package cn.edu.gittest
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private  val REQUEST_CODE_SPEECH_INPUT=100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        voiceBth.setOnClickListener {
+            speak();
+        }
     }
+
+    private fun speak() {
+        val mIntent = Intent (RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hi speak something")
+
+        try {
+
+            startActivityForResult(mIntent,REQUEST_CODE_SPEECH_INPUT)
+        }
+        catch (e:Exception){
+            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode){
+            REQUEST_CODE_SPEECH_INPUT -> {
+                if (resultCode == Activity.RESULT_OK && null != data){
+
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+
+                    textTv.text = result!![0]
+                }
+            }
+        }
+    }
+
+
 }
